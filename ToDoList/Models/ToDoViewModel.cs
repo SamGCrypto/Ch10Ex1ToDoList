4b54876d8e31bd163c1d92b7577d75db9975f34a
samgcrypto <skyrealmoblivion@gmail.com>
2023-11-12 21:09:39 -0500

    Add project files.

---
+++ ToDoList/Models/ToDoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Models
+{
+    public class ToDoViewModel
+    {
+        public Filters Filters { get; set; } = null;
+        public List<Status> Statuses { get; set; } = null;
+        public List<Category> Categories { get; set; } = null;
+        public Dictionary<string, string> DueFilters { get; set; } = null;
+        public List<ToDo> Task { get; set; } = null;
+        public ToDo CurrentTask { get; set; } = null;
+
+    }
+}
